f19066d4cb4bbe633045394c1c19ef7fbc285a5c
Athon <93590599+7c1ptk1dd1e@users.noreply.github.com>
2021-11-02 13:13:51 +0100

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,17 @@
+
+import typer
+
+app = typer.Typer()
+
+@app.command()
+def hello():
+    print("hello")
+
+@app.command()
+def goodbye():
+    print("Goodbye")
+
+
+
+if  __name__ == "__main__":
+    app()
